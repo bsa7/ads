dc04ac804f1257fcb07147aa82920ef16971680f
Sergeij Belevskij <bsa7@inbox.ru>
2014-10-01 11:52:51 +0600

    code improvement

--- app/controllers/classified_ads_controller.rb
+++ app/controllers/classified_ads_controller.rb
@@ -3,44 +3,42 @@ class ClassifiedAdsController < ApplicationController
 	include ApplicationHelper
 	include ActionController::Live
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def index_channel
 		notify_client
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def init_index_channel
 		response.headers['Content-Type'] = 'text/event-stream'
 		response.headers['Cache-Control'] = 'no-cache'
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def last_ads_datetime
 		ClassifiedAd.redis.hget "LatestAds", "datetime"
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def notify_client
 		init_index_channel
 		ads_timestamp = last_ads_datetime #this var used because IOError will be trown if try to use call from stream.write moment
 		begin
 			response.stream.write("id: #{Time.now}\n\n")
-			response.stream.write("retry: 5000\n\n")
+			response.stream.write("retry: 1000\n\n")
 			response.stream.write("data: #{ads_timestamp}\n\n")
 		rescue => e
 			Rails.logger.debug "Raise error at notify_client #{'=='*88}"
 			Rails.logger.debug e.inspect
 		end
-#		close_index_channel
-#		render nothing: true
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def close_index_channel
 		response.stream.close
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def create
 		ActiveRecord::Base.transaction do
 			@ads = ClassifiedAd.create(
@@ -60,7 +58,7 @@ class ClassifiedAdsController < ApplicationController
 		render json: {ads_text: params[:ads_text], other: params}
 	end
 
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def index
 		if params && params[:timezone]
 			@client_timezone = params[:timezone] || "UTC"
@@ -69,24 +67,21 @@ class ClassifiedAdsController < ApplicationController
 		if params && params[:count]
 			count = params[:count] || Count_of_ads
 		end
-		if params && params[:older_than]
-			@classified_ads = ClassifiedAd.where("created_at < '#{date}'").order('created_at DESC').limit(Count_of_ads)
-		elsif params && params[:later_than]
-			@classified_ads = ClassifiedAd.where("created_at > '#{date}'").order('created_at DESC').limit(Count_of_ads)
+		if params && (params[:older_than] || params[:later_than])
+			@classified_ads = ClassifiedAd.where("created_at #{params[:older_than] ? '<' : '>'} '#{date}'").order('created_at DESC').limit(Count_of_ads)
 		else
 			@classified_ads = ClassifiedAd.order('created_at DESC').limit(Count_of_ads)
 		end
 		render layout: need_layout(params)
 	end
 	
- # ***********************************************************************************************
+# ***********************************************************************************************
 	def show
 		set_ad
 		render layout: need_layout(params)
 	end
 
-
- # ***********************************************************************************************
+# ***********************************************************************************************
 	private
 
 	def set_ad

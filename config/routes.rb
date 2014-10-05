96888201a886c50d1522a003b34badad932a50ba
Sergeij Belevskij <bsa7@inbox.ru>
2014-10-06 01:14:41 +0600

    move root to sub url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-	post '/upload_image' => 'images#upload'
-	post '/add_ads' => 'classified_ads#create'
-	get '/ad/:id(.:format)' => 'classified_ads#show'
-	get '/index_channel' => 'classified_ads#index_channel'
-	get '/close_index_channel' => 'classified_ads#close_index_channel'
-	root 'classified_ads#index'
+	scope "/ads" do
+		post '/upload_image' => 'images#upload'
+		post '/add_ads' => 'classified_ads#create'
+		get '/ad/:id(.:format)' => 'classified_ads#show'
+		get '/index_channel' => 'classified_ads#index_channel'
+		get '/close_index_channel' => 'classified_ads#close_index_channel'
+		root 'classified_ads#index'
+	end
 end

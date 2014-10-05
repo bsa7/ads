3381154519ff7478c0b0170ae768add7db3953ba
Sergeij Belevskij <bsa7@inbox.ru>
2014-10-06 01:47:24 +0600

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-	scope "/объявления" do
-		post '/upload_image' => 'images#upload'
-		post '/add_ads' => 'classified_ads#create'
-		get '/ad/:id(.:format)' => 'classified_ads#show'
-		get '/index_channel' => 'classified_ads#index_channel'
-		get '/close_index_channel' => 'classified_ads#close_index_channel'
-		root 'classified_ads#index'
-	end
+	post '/upload_image' => 'images#upload'
+	post '/add_ads' => 'classified_ads#create'
+	get '/ad/:id(.:format)' => 'classified_ads#show'
+	get '/index_channel' => 'classified_ads#index_channel'
+	get '/close_index_channel' => 'classified_ads#close_index_channel'
+	root 'classified_ads#index'
 end
